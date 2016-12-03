b75b76a20aa8aa35781a77ac885e41dac91286b9
oliverlai55 <oliverlai55@gmail.com>
2016-12-02 22:37:16 -0500

    Update course status

--- notes-node/app.js
+++ notes-node/app.js
@@ -17,9 +17,7 @@ if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log("Note created");
-    console.log("==========");
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log("Note title taken");
   }
@@ -27,9 +25,17 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log("Note Found");
+    notes.logNote(note);
+  } else {
+    console.log('note not found');
+  }
 } else if (command = 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else {
   console.log('Command not recognized');
 }
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+  name: 'Oliver'
+};
+
+person.age = 25;
+
+person.name = 'Mike';
+
+console.log(person);
