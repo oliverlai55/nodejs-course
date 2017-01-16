12c922d41deb3dabbb85cf869c0ef698a92c2496
oliverlai55 <oliverlai55@gmail.com>
2017-01-16 03:43:50 -0500

    Refactor code

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,21 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode.js')
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
 // geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 //   if (errorMessage) {
 //     console.log(errorMessage);
@@ -23,19 +24,4 @@
 //   }
 // });
 
-//5d7d7fe29b94abeb084c41a5ac925f43
-//https://api.darksky.net/forecast/5d7d7fe29b94abeb084c41a5ac925f43/40.7449081,-73.95692249999999
-
-const request = require('request');
-
-request({
-  url: 'https://api.darksky.net/forecast/5d7d7fe29b94abeb084c41a5ac925f43/40.7449081,-73.95692249999999',
-  json: true
-}, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(body.currently.temperature);
-  } else {
-    console.log('Unable to fetch weather');
-  }
-
-});
+weather.getWeather();
