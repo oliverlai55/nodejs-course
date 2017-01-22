b614e4b6bf7ae49a06dfb06bda584243f181204e
oliverlai55 <oliverlai55@gmail.com>
2017-01-22 15:05:34 -0500

    Refactor test files

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,19 +1,37 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`)
-  }
+  expect(res).toBe(44).toBeA('number');
+
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`)
+  // }
 });
 // BDD Beahvior drivein devlopemtn
 
 
 it('should square 2 numbers', () => {
-  var res2 = utils.square(2);
+  var res2 = utils.square(3);
 
-  if (res2 !== 9) {
-    throw new Error(`Expected 9, but got ${res2}`)
-  }
+  expect(res).toBe(9).toBeA('number');
+  // if (res2 !== 9) {
+  //   throw new Error(`Expected 9, but got ${res2}`)
+  // }
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11);
+  expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+  expect([2,3,4]).toExclude(5);
+  expect({
+    name: 'Andrew',
+    age: '25',
+    location: 'New York'
+  }).toInclude({
+    age: '25'
+  })
 });
