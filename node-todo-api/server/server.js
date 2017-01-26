edf6aee75f38404a48e0fca93239ef1a321384e7
oliverlai55 <oliverlai55@gmail.com>
2017-01-26 00:17:25 -0500

    Refactor code

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,68 +1,18 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    //value must exiast in required
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-//
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-//
-// var otherTodo = new Todo({
-//   text: 'Edit the video',
-//   completed: true,
-//   completedAt: 0
-// });
-//
-// otherTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save');
-// });
+var app = express();
 
+app.post('/todos', (req, res) => {
 
-// Another Example
-// new User model
-//email: require it, trim it, type to string, set min length of 1
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
 });
 
-var newUser = new User({
-  email: "oliver.lai@hotmail.com"
-});
+//GET
 
-newUser.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save', e);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
