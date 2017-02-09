83c5df5941914a254fda13a5ae38bc78137a945a
oliverlai55 <oliverlai55@gmail.com>
2017-02-08 21:19:09 -0500

    Add user schema

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -51,6 +51,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch(e) {
+
+  }
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
