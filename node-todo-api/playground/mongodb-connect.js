ca534f80fae4406de298569fd0116a0bda73fa00
oliverlai55 <oliverlai55@gmail.com>
2018-01-09 19:48:59 -0500

    Add async await

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,5 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-// const {MongoClient, ObjectID} = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if (err) {
--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -2,33 +2,34 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 // const {MongoClient, ObjectID} = require('mongodb');
 
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-  if (err) {
-    return console.log('Unable to connect to MondoDB server');
-  }
-  console.log('Connected to MongoDB server');
+	if (err) {
+		return console.log('Unable to connect to MondoDB server');
+	}
+	console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').find({
-  //   _id: new ObjectID('58852de8618566529a5f8fd7')
-  // }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to fetch todo', err);
-  // });
+	// db.collection('Todos').find({
+	//   _id: new ObjectID('58852de8618566529a5f8fd7')
+	// }).toArray().then((docs) => {
+	//   console.log('Todos');
+	//   console.log(JSON.stringify(docs, undefined, 2));
+	// }, (err) => {
+	//   console.log('Unable to fetch todo', err);
+	// });
 
+	// db.collection('Todos').find().count().then((count) => {
+	//   console.log(`Todos count: ${count}`);
+	//   console.log(JSON.stringify(count, undefined, 2));
+	// }, (err) => {
+	//   console.log('Unable to fetch todo', err);
+	// });
 
-  // db.collection('Todos').find().count().then((count) => {
-  //   console.log(`Todos count: ${count}`);
-  //   console.log(JSON.stringify(count, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to fetch todo', err);
-  // });
-
-  db.collection('Users').find({name: 'Oliver'}).toArray().then((docs) => {
-
-    console.log(JSON.stringify(docs, undefined, 2));
-  });
-  // db.close();
+	db
+		.collection('Users')
+		.find({ name: 'Oliver' })
+		.toArray()
+		.then(docs => {
+			console.log(JSON.stringify(docs, undefined, 2));
+		});
+	// db.close();
 });
