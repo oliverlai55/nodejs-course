ad5011742116e34a1873960d857285fc29b8cc51
Oliver Lai <oliverlai55@gmail.com>
2018-01-10 17:49:29 -0500

    Add more promises

--- async-await/app-promises.js
+++ async-await/app-promises.js
@@ -11,7 +11,19 @@ const users = [
 	},
 ];
 
-const grades = [];
+const grades = [{
+	id: 1,
+	schoolId: 101,
+	grade: 86
+}, {
+	id: 2,
+	schoolId: 999,
+	grade: 100
+}, {
+	id: 3,
+	schoolId: 101,
+	grade: 80
+}];
 
 const getUser = id => {
 	return new Promise((resolve, reject) => {
@@ -25,9 +37,32 @@ const getUser = id => {
 	});
 };
 
-getUser(2)
-	.then(user => {
-		console.log(user);
+const getGrades = (schoolId) => {
+	return new Promise((resolve, reject) => {
+		resolve(grades.filter((grade) => grade.schoolId === schoolId))
+	});
+};
+
+// Andrew has a 83% in the class
+const getStatus = (userId) => {
+	let user;
+	return getUser(userId).then((tempUser) => {
+		user = tempUser;
+		return getGrades(user.schoolId);
+	}).then((grades) => {
+		// average
+		// return our string
+		let average = 0;
+
+		if (grades.length > 0) {
+			average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+		}
+		return `${user.name} has a ${average}% in the class.`;
+	})
+};
+
+getStatus(1).then(status => {
+		console.log(status);
 	})
 	.catch(e => {
 		console.log(e);
