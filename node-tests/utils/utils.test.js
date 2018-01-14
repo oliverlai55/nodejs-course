8a8e93f28f4cb4fcaff0e210be1fa180286cfe09
oliverlai55 <oliverlai55@gmail.com>
2018-01-14 03:13:43 -0500

    Add tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,35 +3,52 @@ const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+	var res = utils.add(33, 11);
 
-  expect(res).toBe(44).toBeA('number');
+	expect(res)
+		.toBe(44)
+		.toBeA('number');
 
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}`)
-  // }
+	if (res !== 44) {
+		throw new Error(`Expected 44, but got ${res}`);
+	}
 });
 // BDD Beahvior drivein devlopemtn
 
-
 it('should square 2 numbers', () => {
-  var res2 = utils.square(3);
+	var res2 = utils.square(3);
 
-  expect(res).toBe(9).toBeA('number');
-  // if (res2 !== 9) {
-  //   throw new Error(`Expected 9, but got ${res2}`)
-  // }
+	expect(res2)
+		.toBe(9)
+		.toBeA('number');
+
+	if (res2 !== 9) {
+		throw new Error(`Expected 9, but got ${res2}`);
+	}
 });
 
-it('should expect some values', () => {
-  // expect(12).toNotBe(11);
-  expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
-  expect([2,3,4]).toExclude(5);
-  expect({
-    name: 'Andrew',
-    age: '25',
-    location: 'New York'
-  }).toInclude({
-    age: '25'
-  })
+// should verify first and last names are set
+// assert it includes firstNAME AND LASTNAME with proper values
+it('should set firstName and lastName', () => {
+	var user = { location: 'Philadelphia', age: 25 };
+	var res = utils.setName(user, 'Oliver Lai');
+
+	expect(user).toEqual(res);
+	expect(res).toInclude({
+		firstName: 'Oliver',
+		lastName: 'Lai',
+	});
 });
+
+// it('should expect some values', () => {
+// expect(12).toNotBe(11);
+// 	expect({ name: 'Andrew' }).toEqual({ name: 'Andrew' });
+// 	expect([2, 3, 4]).toExclude(5);
+// 	expect({
+// 		name: 'Andrew',
+// 		age: '25',
+// 		location: 'New York',
+// 	}).toInclude({
+// 		age: '25',
+// 	});
+// });
